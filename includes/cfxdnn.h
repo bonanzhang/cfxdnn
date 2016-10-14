e9275885af5d487070d2d8e8a9b65fe3de26dca4
Bonan Zhang <bonan.zhang@gmail.com>
2016-10-14 14:13:26 -0700

    one include file to rule them all

---
+++ includes/cfxdnn.h
@@ -0,0 +1,8 @@
+#ifndef CFXDNN_H
+#define CFXDNN_H
+#include "sequential_network.h"
+#include "fully_connected_layer.h"
+#include "relu_layer.h"
+#include "sgd.h"
+#include "softmax_objective.h"
+#endif // CFXDNN_H
