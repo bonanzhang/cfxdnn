e2c6e5f7bb3e28f57ec1bdc2a0b5517b0a087231
Bonan Zhang <bonan.zhang@gmail.com>
2016-10-25 11:40:02 -0700

    fixed ground truth tensor size

--- main.cc
+++ main.cc
@@ -7,57 +7,22 @@ int main() {
   size_t const input_c= 3;
   size_t const input_h = 224;
   size_t const input_w = 224;
-  size_t const n_classes = 1000;
+  size_t const n_classes = 10;
   float *inputData = static_cast<float *>(malloc(sizeof(float)*batch_size*input_c*input_h*input_w));
   std::cout << "inputData allocated at " << static_cast<void*>(inputData) << std::endl;
   srand(0);
   for(int i = 0; i < batch_size*input_c*input_h*input_w; i++) {
     inputData[i] = ((float)rand())/((float) RAND_MAX)-0.5;
   }
-  std::vector<size_t> ground_truth(1000, 0);
+  std::vector<size_t> ground_truth(batch_size, 0);
   SoftMaxObjective obj;
   SGD sgd;
 
   SequentialNetwork net(batch_size, input_c, input_h, input_w, n_classes);
   net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 64, false)); 
   net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 64, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
   net.add_layer(new MaxPoolLayer(2,2, 2,2, 0,0)); 
-
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 128, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 128, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new MaxPoolLayer(2,2, 2,2, 0,0)); 
-
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 256, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 256, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 256, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new MaxPoolLayer(2,2, 2,2, 0,0)); 
-
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 512, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 512, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 512, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new MaxPoolLayer(2,2, 2,2, 0,0)); 
-
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 512, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 512, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new ConvolutionLayer(3,3, 1,1, 1,1, 512, false)); 
-  net.add_layer(new ReLULayer(0.0f)); 
-  net.add_layer(new MaxPoolLayer(2,2, 2,2, 0,0)); 
-
-  net.add_layer(new FullyConnectedLayer(4096, false)); 
-  net.add_layer(new FullyConnectedLayer(4096, false)); 
-  net.add_layer(new FullyConnectedLayer(1000, false)); 
+  net.add_layer(new FullyConnectedLayer(n_classes, false)); 
   std::cout << "Finalizing Layers" << std::endl;
   net.finalize_layers();
   std::cout << "Starting training" << std::endl;
@@ -68,9 +33,9 @@ int main() {
     float loss = net.getLoss(obj, ground_truth);
     std::cout << "loss calculation complete" << std::endl;
     std::cout << loss << std::endl;
-//    std::cout << "starting backward pass" << std::endl;
-//    net.backward();
-//    std::cout << "backward pass complete" << std::endl;
+    std::cout << "starting backward pass" << std::endl;
+    net.backward();
+    std::cout << "backward pass complete" << std::endl;
 //    net.update(sgd, 0.001f);
 //    std::cout << "update complete" << std::endl;
   }
